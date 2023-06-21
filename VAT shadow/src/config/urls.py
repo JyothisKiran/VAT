6d17e0f5005363f1d00ecefc179a91521cdc21c9
JyothisKiran <jyothiskirandc007@gmail.com>
2023-06-21 17:47:18 +0530

    VAT dashboard

--- VAT shadow/src/config/urls.py
+++ VAT shadow/src/config/urls.py
@@ -6,6 +6,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('users.urls')),
+    path('contrib/',include('contrib.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- VAT shadow/src/contrib/views.py
+++ VAT shadow/src/contrib/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class UserDashboardView(TemplateView):
+    template_name = 'contrib/base.html'
