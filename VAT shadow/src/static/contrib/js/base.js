33a641390ad621f239074c9a45bb9424d0753fe8
JyothisKiran <jyothiskirandc007@gmail.com>
2023-06-22 17:22:31 +0530

    dashboard 75% done

--- VAT shadow/src/static/contrib/js/base.js
+++ VAT shadow/src/static/contrib/js/base.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
-    {
-        let sideBar = document.querySelector('.side-bar');
-        let arrowCollapse = document.querySelector('#logo-name__icon');
-        sideBar.onclick = () => {
-          sideBar.classList.toggle('collapse');
-          arrowCollapse.classList.toggle('collapse');
-          if (arrowCollapse.classList.contains('collapse')) {
-            arrowCollapse.classList =
-              'bx bx-arrow-from-left logo-name__icon collapse';
-          } else {
-            arrowCollapse.classList = 'bx bx-arrow-from-right logo-name__icon';
-          }
-        };
-      }
+  $('.collapse-content').show();
+  $('.right_arrow').hide();
+  $("#collapse_btn").click(function () {
+    // Toggle visibility of the collapse content
+    if($('.collapse-content').css({'display':'block'})){
+      console.log('expanded');
+      $('.right_arrow').show();
+      $('.left_arrow').hide();
+      $('.navbarContents').css({'width':'87.7px'})
+      $('.collapse-content').hide();
+      $('.sidebar').css({'width':'56px', "transition": "0.7s" });
+      $('li').css({'width':'38px'})
+      $("#collapse_btn").css({'margin-left':'31px',"transition": "0.7s" });
+    }
+    // if ($('.collapse-content').css({'display':'none'})) {
+    //   console.log('collapsed');
+    //   $('.collapse-content').show();
+    // }
+    else{
+      $('.collapse-content').show();
+    };
+  });
+       
 })
