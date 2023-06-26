0772cde15bb875020775b42a00a513f1f7b3a80d
JyothisKiran <122264774+JyothisKiran@users.noreply.github.com>
2023-06-26 09:17:58 +0530

    Assettype ListView

--- VAT shadow/src/asset_type/urls.py
+++ VAT shadow/src/asset_type/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AssettypeListView
+from .views import AssettypeListView,AssettypeAddView
 
 urlpatterns = [
     path('list/',AssettypeListView.as_view(),name='list'),
+    path('add/',AssettypeAddView.as_view(),name='add'),
 ]
--- VAT shadow/src/asset_type/views.py
+++ VAT shadow/src/asset_type/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
 from .models import AssetType
 # Create your views here.
 
 class AssettypeListView(ListView):
     model = AssetType
     template_name = 'asset_type/list.html'
+
+
+class AssettypeAddView(CreateView):
+    model = AssetType
+    fields = ['type_name','description']
+    template_name = 'asset_type/add.html'
     
